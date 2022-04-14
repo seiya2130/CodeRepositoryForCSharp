c9b5a52fa1e4d1a5300d8204d2b6e87be56daafb
Seiya_Satoh <satohseiya0312@gmail.com>
2022-04-14 09:36:29 +0900

    [add]インターフェース追加

---
+++ CodeRepositoryForCSharp/IEmployee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeRepositoryForCSharp
+{
+    interface IEmployee
+    {
+        public List<Employee> GetEmployees();
+    }
+}
