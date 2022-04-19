93f0962fbea38e8d843de5e94854c40bbe112378
Seiya_Satoh <satohseiya0312@gmail.com>
2022-04-19 09:37:56 +0900

    [更新]手動でDI

--- CodeRepositoryForCSharp/Employee.cs
+++ CodeRepositoryForCSharp/Employee.cs
@@ -4,11 +4,16 @@ namespace CodeRepositoryForCSharp
 {
     class Employee : IEmployee
     {
+        public int No;
         public List<Employee>GetEmployees()
         {
             var employeeList = new List<Employee>();
 
-            //DBから従業員を取得したと仮定する
+            //DBから従業員を10人取得したと仮定する
+            for (int i = 0; i < 10; i++)
+            {
+                employeeList.Add(new Employee() { No = i });
+            }
 
             return employeeList;
         }
