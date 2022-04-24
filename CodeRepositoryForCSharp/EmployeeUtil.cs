ac94dc95f5e75064f249c54266c4a69fed2b2495
Seiya_Satoh <satohseiya0312@gmail.com>
2022-04-24 17:14:51 +0900

    [add]Utilクラス追加、DIコンテナ追加

---
+++ CodeRepositoryForCSharp/EmployeeUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeRepositoryForCSharp
+{
+    class EmployeeUtil : IEmployee
+    {
+        public List<Employee> GetEmployees() {
+            var employeeList = new List<Employee>();
+
+            //DBから従業員を10人取得したと仮定する
+            for (int i = 0; i < 10; i++)
+            {
+                employeeList.Add(new Employee() { No = i });
+            }
+
+            return employeeList;
+        }
+    }
+}
