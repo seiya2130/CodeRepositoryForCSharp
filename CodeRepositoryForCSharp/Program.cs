338e613de3f3c8f271820ab51debec5d1b4c5969
Seiya_Satoh <satohseiya0312@gmail.com>
2022-05-02 09:32:00 +0900

    [update]微修正

--- CodeRepositoryForCSharp/Program.cs
+++ CodeRepositoryForCSharp/Program.cs
@@ -8,15 +8,19 @@ namespace CodeRepositoryForCSharp
         static void Main(string[] args)
         {
             var services = new ServiceCollection();
+
+            //事前に依存関係を登録
             services.AddSingleton<IEmployee>(new Employee());
             var provider = services.BuildServiceProvider();
 
+            //サービスの型を取得(ここではEmployeeクラスを取得)
             var employee = provider.GetRequiredService<IEmployee>();
             var manager = new Manager(employee);
 
             foreach (var staff in manager.GetStaff())
             {
-                Console.WriteLine(staff.No);
+                Console.Write(staff.No);
+                //結果 12345
             }
 
         }
