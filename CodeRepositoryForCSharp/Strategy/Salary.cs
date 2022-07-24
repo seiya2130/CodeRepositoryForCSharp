42c192298a53234b3706cf8d4b044944d86bf805
Seiya_Satoh <satohseiya0312@gmail.com>
2022-07-24 15:56:57 +0900

    リファクタ前追加

---
+++ CodeRepositoryForCSharp/Strategy/Salary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeRepositoryForCSharp.Strategy
+{
+    class Salary
+    {
+
+        public Salary()
+        {
+
+        }
+
+        public double GetSalary(int userId, WorkStatus workStatus)
+        {
+            var salary = (double)200_000;
+
+            switch (workStatus)
+            {
+                case WorkStatus.Full:
+                    break;
+                case WorkStatus.Short:
+                    salary = salary * 0.8;
+                    break;
+                case WorkStatus.Leave:
+                    salary = salary * 0.6;
+                    break;
+            }
+
+            return salary;
+        }
+    }
+}
+
+public enum WorkStatus
+{
+    Full,
+    Short,
+    Leave
+}
