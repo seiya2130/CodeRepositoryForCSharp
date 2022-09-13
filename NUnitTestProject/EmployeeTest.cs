3664ed02c7c44acc4954ae5665fd94fff15f2ec3
Seiya_Satoh <satohseiya0312@gmail.com>
2022-09-13 09:45:51 +0900

    mockテスト追加

---
+++ NUnitTestProject/EmployeeTest.cs
@@ -0,0 +1,25 @@
+﻿using CodeRepositoryForCSharp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject
+{
+    class EmployeeTest
+    {
+        [Test]
+        public void GetEmployeesTest()
+        {
+            Moq.Mock<IEmployee> moq = new Moq.Mock<IEmployee>();
+            var mockEmployeeList = new List<Employee>()
+            {
+                new Employee(),
+            };
+
+            moq.Setup(x => x.GetEmployees()).Returns(mockEmployeeList);
+
+
+        }
+    }
+}
