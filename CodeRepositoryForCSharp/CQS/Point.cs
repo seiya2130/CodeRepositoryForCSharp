ebaacb4b45f34a8ba7e5a91fdbc259c5e851fa90
Seiya_Satoh <satohseiya0312@gmail.com>
2022-09-19 22:12:32 +0900

    CQS変更前

---
+++ CodeRepositoryForCSharp/CQS/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeRepositoryForCSharp.CQS
+{
+    class Point
+    {
+        int ThanksPoint { get; set; }
+
+        public int AddThanksPoint(int addPoint)
+        {
+            ThanksPoint += addPoint;
+            return  ThanksPoint;
+        }
+    }
+}
