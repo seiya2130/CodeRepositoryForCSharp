85b46728f591734220556c94bc18582155b26ee3
Seiya_Satoh <satohseiya0312@gmail.com>
2022-09-25 21:16:09 +0900

    変更前

---
+++ CodeRepositoryForCSharp/CompleteConstructor/CompleteConstructor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+using System.Web;
+
+namespace CodeRepositoryForCSharp.CompleteConstructor
+{
+    class CompleteConstructor : Controller
+    {
+
+        [HttpPost]
+        public ActionResult RegisterSkill(Skill skill)
+        {
+            ValidateSkill(skill);
+            
+            // 問題なければ登録処理
+
+            return View();
+        }
+
+        private void ValidateSkill(Skill skill)
+        {
+            if (skill.Score < 0)
+            {
+                throw new ValidationException("スコアが不正です");
+            }
+        }
+    }
+
+    class RegisterSkillRequest
+    {
+        public Skill skill { get; set; }
+    }
+
+    class Skill
+    {
+        public int Score { get; set; }
+    }
+
+    class ValidationException : Exception
+    {
+        public ValidationException(string message)
+        {
+
+        }
+    }
+}
