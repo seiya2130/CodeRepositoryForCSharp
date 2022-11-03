b0544902413c381a52c3e8737f3564e5c551e36d
Seiya_Satoh <satohseiya0312@gmail.com>
2022-11-03 21:10:43 +0900

    DTO修正

---
+++ CodeRepositoryForCSharp/DTO/SettingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace CodeRepositoryForCSharp.DTO
+{
+    class SettingController : Controller
+    {
+
+        private SettingRepository settingRepository;
+
+        [HttpPost]
+        public ActionResult UpdateSetting(UpdateSettingRequest updateSettingRequest)
+        {
+            settingRepository.UpdateSetting(updateSettingRequest);
+            return View();
+        }
+    }
+
+    class SettingRepository
+    {
+        public void UpdateSetting(UpdateSettingRequest updateEnqueteSettingRequest)
+        {
+
+        }
+    }
+
+    class UpdateSettingRequest
+    {
+        int settingId { get; set; }
+        string settingName { get; set; }
+        int authorUserId { get; set; }
+        Boolean isEnable { get; set; }
+        List<int> targetUserIdList { get; set; }
+    }
+
+}
