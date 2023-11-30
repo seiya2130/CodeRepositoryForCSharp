d0207fcf35d30adb825c5e3731c0814310715a97
Seiya_Satoh <satohseiya0312@gmail.com>
2023-11-30 09:51:15 +0900

    プロダクションコード作成

---
+++ CodeRepositoryForCSharp/TemplateMethod/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeRepositoryForCSharp.TemplateMethod
+{
+    public abstract class Character
+    {
+        public abstract string Name { get; }
+        public abstract int AttackPoint { get; }
+        public void Attack()
+        {
+            Console.WriteLine($"{Name}のこうげき！");
+            Console.WriteLine($"あいてモンスターに{AttackPoint}のダメージ");
+        }
+    }
+}
---
+++ CodeRepositoryForCSharp/TemplateMethod/Wizard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeRepositoryForCSharp.TemplateMethod
+{
+    public class Wizard : Character, IAttackCommand
+    {
+        public override string Name => "魔法使い";
+        public override int AttackPoint => 50;
+    }
+}
